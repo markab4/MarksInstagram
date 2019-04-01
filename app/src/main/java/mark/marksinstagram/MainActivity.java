034ad54962d3f8bda7c36e3a1a7a5ecb0ed15f42
Mark Abramov <markabramov01@gmail.com>
2019-04-01 11:34:21 -0400

    define frame layout for fragments

--- app/src/main/java/mark/marksinstagram/MainActivity.java
+++ app/src/main/java/mark/marksinstagram/MainActivity.java
@@ -3,45 +3,56 @@ package mark.marksinstagram;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import mark.marksinstagram.fragments.ComposeFragment;
+import mark.marksinstagram.fragments.PostsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     public final String TAG = "MainActivity";
 
-
     private BottomNavigationView bottomNavigationView;
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final FragmentManager fragmentManager = getSupportFragmentManager();
 
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
                 switch (menuItem.getItemId()) {
                     case R.id.action_home:
+                        // TODO: swap fragment here
+                        fragment = new PostsFragment();
                         Toast.makeText(MainActivity.this, "Home!", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_compose:
+                        fragment = new ComposeFragment();
                         Toast.makeText(MainActivity.this, "Compose!", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_profile:
                     default:
+                        // TODO: swap fragment here
+                        fragment = new ComposeFragment();
                         Toast.makeText(MainActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
                         break;
                 }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                 return true;
             }
         });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 
 
---
+++ app/src/main/java/mark/marksinstagram/fragments/PostsFragment.java
@@ -0,0 +1,22 @@
+package mark.marksinstagram.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mark.marksinstagram.R;
+
+public class PostsFragment extends Fragment {
+    // onCreateView to inflate the view
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_posts, container, false);
+    }
+}
